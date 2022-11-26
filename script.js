5bd1f88fa2176dd9a8c6f7f8bec2b6feec413817
Zehralog <116207798+Zehralog@users.noreply.github.com>
2022-11-26 19:27:57 +0300

    Random-Code-new

--- script.js
+++ script.js
@@ -21,3 +21,16 @@ function copyCode() {
     copyText.setSelectionRange(0, 999);
     document.execCommand("copy");
 }
+
+function genClick() {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var codeLength = 5;
+    var code = "CP";
+
+    for (var i = 0; i <= codeLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        code += chars.substring(randomNumber, randomNumber + 1);
+    }
+
+    document.getElementById("code").value = code;
+}
