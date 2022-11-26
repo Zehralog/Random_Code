3ac1f0c87f7f554283a989e5ee8ed9e03cbb7d03
Zehralog <116207798+Zehralog@users.noreply.github.com>
2022-11-26 17:06:40 +0300

    Random-Code

---
+++ script.js
@@ -0,0 +1,23 @@
+var code = document.getElementById("code");
+
+function genCode() {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var codeLength = 5;
+    var code = "CP";
+
+    for (var i = 0; i <= codeLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        code += chars.substring(randomNumber, randomNumber + 1);
+    }
+
+    document.getElementById("code").value = code;
+
+}
+
+
+function copyCode() {
+    var copyText = document.getElementById("code");
+    copyText.select();
+    copyText.setSelectionRange(0, 999);
+    document.execCommand("copy");
+}
